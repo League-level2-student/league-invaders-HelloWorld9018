f34599a2733768120e2e6247a0a15ae08cf1c31b
Lily Cov <lily@bettersites.com>
2021-04-22 17:39:03 -0700

    Images loaded successfully.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,12 +5,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
+	
 	final int MENU = 0;
 	final int GAME = 1;
 	final int END = 2;
@@ -25,7 +31,9 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	
 	Rocketship rocket = new Rocketship(250, 700, 50, 50);
 	
-	boolean inBounds;
+//	boolean inBounds;
+	
+	ObjectManager manager = new ObjectManager(rocket);
 	
 	GamePanel(){
 		titleFont = new Font("Serif", Font.BOLD, 45);
@@ -34,6 +42,11 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		
 		frameDraw = new Timer(1000/60, this);
 		frameDraw.start();
+		
+		if (needImage) {
+		    loadImage ("space.png");
+		}
+		
 	}
 	
 	void updateMenuState(){
@@ -41,7 +54,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	}
 	
 	void updateGameState(){
-		
+		manager.update();
 	}
 	
 	void updateEndState(){
@@ -63,9 +76,14 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	}
 
 	void drawGameState(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-		rocket.draw(g);
+
+		if (gotImage) {
+			g.drawImage(image, 0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT, null);
+		} else {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		}
+		manager.draw(g);
 	}
 	
 	void drawEndState(Graphics g) {
@@ -169,8 +187,20 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		
 	}
 	
+	void loadImage(String imageFile) {
+	    if (needImage) {
+	        try {
+	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+		    gotImage = true;
+	        } catch (Exception e) {
+	            System.out.println("There is an error in loading the background image");
+	        }
+	        needImage = false;
+	    }
+	}
+	
 	//This is not needed, delete maybe
-	void checkBounds() {
+/*	void checkBounds() {
 		if(rocket.x > 0 && rocket.x < 500 && rocket.y > 0 && rocket.y < 800) {
 			inBounds = true;
 		}
@@ -178,5 +208,5 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 			inBounds = false;
 		}
 	}
-	
+	*/
 }
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -17,6 +18,41 @@ public class ObjectManager {
 	
 	void addAleins(){
 		alienTroops.add(new Alien(ran.nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+		
 	}
-	//left on step 5
+
+	void update() {
+		for(int i = 0; i<alienTroops.size(); i++) {
+			alienTroops.get(i).update();
+			if(alienTroops.get(i).height > LeagueInvaders.HEIGHT) {
+				alienTroops.get(i).isActive = false;
+			}
+		
+		}
+	}
+	
+	void draw(Graphics g) {
+		rocket.draw(g);
+		for(int i = 0; i < alienTroops.size(); i++) {
+			alienTroops.get(i).draw(g);
+		}
+		for(int i = 0; i < missiles.size(); i++) {
+			missiles.get(i).draw(g);
+		}
+	}
+	
+	void purgeObjects() {
+		for(int i = 0; i < alienTroops.size(); i++) {
+			if(alienTroops.get(i).isActive = false) {
+				alienTroops.remove(i);
+			}
+		}
+		for(int i = 0; i < missiles.size(); i++) {
+			if(missiles.get(i).isActive = false) {
+			missiles.remove(i);
+			}
+		}
+	}
+	
+	
 }
